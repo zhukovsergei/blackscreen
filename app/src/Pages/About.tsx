6c8570ff7cca21dff544b4db883259728156129c
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-03-08 03:55:12 +0100

    Added about description

--- app/src/Pages/About.tsx
+++ app/src/Pages/About.tsx
@@ -3,9 +3,9 @@ import { Container, Title, Text } from '@mantine/core';
 export function About() {
     return (
         <Container>
-            <Title order={2}>О приложении</Title>
-            <Text mt="md">
-                Это страница «О приложении». Здесь вы можете узнать больше о целях и функциональности данного проекта.
+            <Title order={1}>About</Title>
+            <Text mt="lg" fz={"xl"}>
+                The project was created to test the screen for burnt pixels, dimming without the off button or use the monitor as a light source. Also rid the users of annoying advertisements, and to keep the fast loading speed of the project.
             </Text>
         </Container>
     );
